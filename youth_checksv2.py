9f992561edeaa7ff4b34460175cf219cb7c680ba
Jim McKeown <mckeown.james@gmailcom>
2023-09-04 12:08:41 +0200

    Initial commit

---
+++ youth_checksv2.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import re
+from datetime import datetime
+
+def update_errors(df, condition, error_message):
+    df.loc[condition, 'error_flag'] = 1
+    df.loc[condition, 'error_description'] += error_message + '; '
+
+def check_required_cols(df, required_cols):
+    for col in required_cols:
+        missing = df[col].isnull()
+        update_errors(df, missing, f'{col} is missing')
+
+
+def check_duplicates(df, duplicate_cols):
+    for col in duplicate_cols:
+        # Create a boolean Series with the same index as df
+        duplicates = df[col].duplicated(keep=False) & df[col].notna() & (df[col] != '')
+        update_errors(df, duplicates, f'Duplicate {col}')
+
+def check_id_numbers(df):
+    no_id = df[(df['RSA ID Number'].isna() | (df['RSA ID Number'] == '')) &
+               (df['Foreign ID Number'].isna() | (df['Foreign ID Number'] == ''))].index
+    update_errors(df, no_id, 'RSA ID Number and Foreign ID Number both blank')
+
+def check_dates(df, date_cols):
+    for col in date_cols:
+        not_blank = df[col].notna() & (df[col] != '')
+        df.loc[not_blank, df.columns[df.columns.get_loc(col)]] = pd.to_datetime(df[not_blank][col], errors='coerce')
+        bad_dates = not_blank & df[col].isnull()
+        update_errors(df, bad_dates, f'{col} is not a valid date')
+
+
+def check_contains(df, contains_checks):
+    for col, char in contains_checks.items():
+        missing_char = ~df[col].apply(lambda x: char in str(x))
+        update_errors(df, missing_char, f'{col} does not contain {char}')
+
+def check_employment_status(df):
+    inactive_without_enddate = (df['Employment Status'] != 'Active') & df['End Date'].isna()
+    update_errors(df, inactive_without_enddate, 'Inactive employment status without End Date')
+
+    end_date_without_reason = df['End Date'].notna() & df['Reason for Leaving'].isna()
+    update_errors(df, end_date_without_reason, 'End Date present but no Reason for Leaving')
+
+def check_age(df):
+    now = pd.Timestamp(datetime.now())
+    df['Age'] = (now - df['DOB']).astype('<m8[Y]')
+    age_invalid = (df['Age'] > 35) | (df['Age'] < 18)
+    update_errors(df, age_invalid, 'Age is either over 35 or under 18')
+
+def check_end_date(df):
+    end_date_filled = df['End Date'].notna()
+    invalid_end_date = end_date_filled & (df['End Date'] < df['Start Date'])
+    update_errors(df, invalid_end_date, 'End Date is before Start Date')
+
+def check_phone_number(df):
+    invalid_phone_number = df['Cell Phone Number'].str.replace('\D', '', regex=True).str.len() != 10
+    update_errors(df, invalid_phone_number, 'Cell Phone Number does not contain exactly 10 digits')
+
+def check_rsa_id_number(df):
+    df_rsa_id = df['RSA ID Number'].notna() & (df['RSA ID Number'] != '')
+    invalid_rsa_id = df_rsa_id & (df['RSA ID Number'].str.len() != 13)
+    update_errors(df, invalid_rsa_id, 'RSA ID Number does not contain exactly 13 digits')
+
+def check_data_integrity(df):
+    required_cols = ['Employee Number', 'Employment Status', 'Mentor', 'Gender', 'Race',
+                 'Site Placement', 'First Names', 'Last Name', 'Job Title',
+                 'Start Date', 'Attachment: ID Document', 'Attachment: Bank Details',
+                 'Attachment: MOU/Contract', 'Attachment: Info Sheet',
+                 'Attachment: SARS Notice of registration', 'Cell Phone Number', 'Email', 'Income Tax Number']
+    duplicate_cols = ['Employee Number', 'Account number', 'RSA ID Number']
+    date_cols = ['Start Date', 'End Date', 'DOB']
+    contains_checks = {'Email': '@'}
+
+    df['error_flag'] = 0
+    df['error_description'] = ''
+
+    check_required_cols(df, required_cols)
+    check_duplicates(df, duplicate_cols)
+    check_id_numbers(df)
+    check_dates(df, date_cols)
+    check_contains(df, contains_checks)
+    check_employment_status(df)
+    check_age(df)
+    check_end_date(df)
+    check_phone_number(df)
+    # check_rsa_id_number(df) -- I'll have to fix this; the .csv from airtable removes leading zeros
+
+    return df
+
+def load_data(filename):
+    all_youth = pd.read_csv(filename, dtype={'RSA ID Number': str})
+    return all_youth[all_youth['Employment Status'] == 'Active'].copy()
+
+def save_errors(df, filename):
+    errors = df[df['error_flag'] == 1]
+    errors.to_csv(filename, index=False)
+
+def main():
+    df = load_data('20230725 - Combined Youth Data-All Yeboneers.csv')
+    df = check_data_integrity(df)
+    save_errors(df, 'youth_errors.csv')
+
+if __name__ == '__main__':
+    main()
---
+++ youth_data 2.py
@@ -0,0 +1,106 @@
+import pandas as pd
+import plotly.express as px
+
+# Import CSV and create initial DFs
+all_youth = pd.read_csv("20230705_youth.csv")
+youth = all_youth[all_youth['Employment Status'] == 'Active'].copy()
+active_youth = youth['Employment Status'].count()
+total_youth = all_youth['Employee Number'].nunique()
+df1 = all_youth[all_youth['Reason for Leaving'] == "Found a Job"]
+found_a_job = df1['Employee Number'].nunique()
+number_of_sites = youth['Site Placement'].nunique()
+# Count the number of youth per area
+area_counts = youth['Area'].value_counts().reset_index()
+area_counts.columns = ['Area', 'Count']
+fig_area = px.bar(area_counts, x='Area', y='Count', color="Area", labels={'Count':'Number of Youths', 'Area': ''}, title='Breakdown by Area')
+
+# Count the number of youth per school
+school_counts = youth['Site Placement'].value_counts().reset_index()
+school_counts.columns = ['Site Placement', 'Count']
+fig_school = px.bar(school_counts, x='Site Placement', y='Count', color="Site Placement", labels={'Count':'Number of Youths', 'School/ECDC': ''}, title='Breakdown by School/ECDC')
+
+# Count the number of youth per race
+race_counts = youth['Race'].value_counts().reset_index()
+race_counts.columns = ['Race', 'Count']
+fig_race = px.pie(race_counts, values='Count', names='Race', title='Breakdown by Race')
+
+# Count the number of youth per race
+gender_counts = youth['Gender'].value_counts().reset_index()
+gender_counts.columns = ['Gender', 'Count']
+fig_gender = px.pie(gender_counts, values='Count', names='Gender', title='Breakdown by Gender')
+
+# Import necessary libraries
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+# Define layout
+app.layout = html.Div([
+
+    html.H1("Youth Dashboard"),
+    html.Br(),
+    html.Br(),
+    html.H2(f"Number of Youth Currently Employed: {str(active_youth)}"),
+    html.Br(),
+    html.H2(f"Number of Youth Employed Past 2.5 Years: {str(total_youth)}"),
+    html.Br(),
+    html.H2(f"Number of Youth Who Have Found Further Employment: {str(found_a_job)}"),
+    html.Br(),
+    html.H2("Youth per Site"),
+    dcc.Dropdown(
+        id='site-dropdown',
+        options=[{'label': i, 'value': i} for i in youth['Site Placement'].unique()],
+        value='Charles Duna P School'
+    ),
+    dcc.Graph(id='site-bar-chart'),
+    html.Br(),
+    html.H2("Youth per Mentor"),
+    dcc.Dropdown(
+        id='mentor-dropdown',
+        options=[{'label': i, 'value': i} for i in youth['Mentor'].unique()],
+        value="Nosisa Ncobo"
+    ),
+    dcc.Graph(id='mentor-bar-chart'),
+    html.Br(),
+    html.H2("Youth by Area"),
+    dcc.Graph(id='area-bar-chart', figure=fig_area),
+    html.Br(),
+    html.H2("Youth by School"),
+    html.H4(f"Number of Sites: {str(number_of_sites)}"),
+    dcc.Graph(id='school-bar-chart', figure=fig_school),
+    html.H2("Youth by Gender"),
+    dcc.Graph(id='gender-bar-chart', figure=fig_gender),
+    html.Br(),
+    html.H2("Youth by Race"),
+    dcc.Graph(id='race-bar-chart', figure=fig_race),
+    html.Br(),
+])
+
+# Define callback
+@app.callback(
+    [Output('site-bar-chart', 'figure'),
+     Output('mentor-bar-chart', 'figure')],
+    [Input('site-dropdown', 'value'),
+     Input('mentor-dropdown', 'value')]
+)
+def update_graph(selected_site, selected_mentor):
+    # First chart based on Site Placement
+    filtered_df_site = youth[youth['Site Placement'] == selected_site]
+    fig1 = px.histogram(filtered_df_site, x='Job Title', color='Job Title',
+                        color_discrete_sequence=px.colors.qualitative.Plotly)
+    fig1.update_layout(showlegend=False)
+
+    # Second chart based on Mentor
+    filtered_df_mentor = youth[youth['Mentor'] == selected_mentor]
+    fig2 = px.histogram(filtered_df_mentor, x='Job Title', color='Job Title',
+                        color_discrete_sequence=px.colors.qualitative.Plotly)
+    fig2.update_layout(showlegend=False)
+
+    return fig1, fig2
+
+# Run app
+if __name__ == '__main__':
+    app.run_server(port=8053)
